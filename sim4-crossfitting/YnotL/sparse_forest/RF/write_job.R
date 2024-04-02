782789e06d8852a832235334c7495669433ccf2b
Anna <guo.anna@qq.com>
2024-04-01 21:19:12 -0400

    keep a record of the tables and plot before densratio change

--- sim4-crossfitting/YnotL/sparse_forest/RF/write_job.R
+++ sim4-crossfitting/YnotL/sparse_forest/RF/write_job.R
@@ -29,6 +29,7 @@ superlearner.M = "T" # whether to use superlearner for M
 superlearner.Y = "T" # whether to use superlearner for Y
 superlearner.A = "T" # whether to use superlearner for A
 crossfit = "F" # number of folds for crossfit
+zerodiv.avoid="0.01"
 
 for (i in seq_along(n.vec)){
   joblist <- c()
@@ -36,7 +37,7 @@ for (i in seq_along(n.vec)){
     job <- paste0("Rscript main.R ",n.vec[i]," ",t," ", dgp.f.name," ",truth," ",out.path.tmle," ", out.path.onestep," ",a," ",
                   vertices," ",di_edges," ",bi_edges," ",treatment," ",outcome," ",
                   multivariate.variables," ",ratio.method.L," ",ratio.method.M," ",lib.seq," ",lib.L," ",lib.M," ",lib.Y," ",lib.A," ",
-                  superlearner.seq," ",superlearner.L," ",superlearner.M," ",superlearner.Y," ",superlearner.A," ",crossfit)
+                  superlearner.seq," ",superlearner.L," ",superlearner.M," ",superlearner.Y," ",superlearner.A," ",crossfit," ", zerodiv.avoid)
     
     joblist <- c(joblist,job)
   }
@@ -55,7 +56,7 @@ for (i in seq_along(n.vec)){
     job <- paste0("Rscript main.R ",n.vec[i]," ",t," ", dgp.f.name," ",truth," ",out.path.tmle," ", out.path.onestep," ",a," ",
                   vertices," ",di_edges," ",bi_edges," ",treatment," ",outcome," ",
                   multivariate.variables," ",ratio.method.L," ",ratio.method.M," ",lib.seq," ",lib.L," ",lib.M," ",lib.Y," ",lib.A," ",
-                  superlearner.seq," ",superlearner.L," ",superlearner.M," ",superlearner.Y," ",superlearner.A," ",crossfit)
+                  superlearner.seq," ",superlearner.L," ",superlearner.M," ",superlearner.Y," ",superlearner.A," ",crossfit," ", zerodiv.avoid)
     joblist <- c(joblist,job)
   }
   write.table(joblist, file = paste0("dnorm_joblist_n",i,".txt") ,quote = F, col.names = F, row.names = F)
@@ -73,7 +74,7 @@ for (i in seq_along(n.vec)){
     job <- paste0("Rscript main.R ",n.vec[i]," ",t," ", dgp.f.name," ",truth," ",out.path.tmle," ", out.path.onestep," ",a," ",
                   vertices," ",di_edges," ",bi_edges," ",treatment," ",outcome," ",
                   multivariate.variables," ",ratio.method.L," ",ratio.method.M," ",lib.seq," ",lib.L," ",lib.M," ",lib.Y," ",lib.A," ",
-                  superlearner.seq," ",superlearner.L," ",superlearner.M," ",superlearner.Y," ",superlearner.A," ",crossfit)
+                  superlearner.seq," ",superlearner.L," ",superlearner.M," ",superlearner.Y," ",superlearner.A," ",crossfit," ", zerodiv.avoid)
     joblist <- c(joblist,job)
   }
   write.table(joblist, file = paste0("densratio_joblist_n",i,".txt") ,quote = F, col.names = F, row.names = F)
